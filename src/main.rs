728af3eb22dbfc4a6ad2a6b90f06bff9c5d65b12
Wulfsta <wulfstawulfsta@gmail.com>
2021-01-16 19:20:59 -0500

    add buffer size option

--- src/main.rs
+++ src/main.rs
@@ -20,77 +20,85 @@ use rand::{
 };
 use std::{
     io,
-    mem,
 };
-use std::fmt::{
-    Debug,
+use std::cell::{
+    RefCell,
 };
 use std::io::{
     Write,
 };
 
-#[derive(Clone, Debug)]
-struct RngContainer<T: Clone + Debug + Rng + SeedableRng> {
-    rng: T,
+
+fn output_bytes<T: Rng + SeedableRng>(rng: &RefCell<T>, n: usize) -> Result<usize, io::Error> {
+    let buf: &mut [u8] = &mut vec![0u8; n][..];
+    rng.borrow_mut().fill_bytes(buf);
+    let num_written = io::stdout().write(&buf)?;
+    Ok(num_written)
 }
 
-impl<T: Clone + Debug + Rng + SeedableRng> RngContainer<T> {
-    fn new(rng_in: T) -> RngContainer<T> {
-        RngContainer { rng: rng_in }
-    }
-
-    fn output_bytes(&mut self) -> Result<usize, io::Error> {
-        io::stdout().write(&self.rng.gen::<usize>().to_ne_bytes())
-    }
-
-    fn output_less_than_usize_bytes(&mut self, n: usize) -> Result<usize, io::Error> {
-        io::stdout().write(&self.rng.gen::<usize>().to_ne_bytes()[0..n])
-    }
-}
 
 fn main() {
     let arg_defs = App::new("psw: prng-stdio-writer")
-                        .version("0.1.2")
+                        .version("0.2.0")
                         .about("Writes output of pcg64 to stdout.")
                         .arg(Arg::with_name("bytes")
                              .short("b")
                              .long("bytes")
                              .takes_value(true)
-                             .help("Number of bytes to write."))
+                             .help("Total number of bytes to write."))
+                        .arg(Arg::with_name("bufsize")
+                             .short("s")
+                             .long("bufsize")
+                             .takes_value(true)
+                             .help("Size in bytes of the buffer for each write. Default is 1024."))
                         .get_matches();
 
     let bytes_exist = arg_defs.value_of("bytes");
    
     //TODO Possibly add more generators.
-    let rng = rand_pcg::Lcg128Xsl64::from_entropy();
-    let mut rng_container = RngContainer::new(rng);
+    thread_local!{
+        static RNG: RefCell<rand_pcg::Lcg128Xsl64> = RefCell::new(rand_pcg::Lcg128Xsl64::from_entropy());
+    };
+
+    let buffer_size = match arg_defs.value_of("bufsize") {
+        Some(buffer_size_as_string) => {
+            buffer_size_as_string.trim().parse::<usize>().expect("Failed to understand supplied value of bufsize")
+        },
+        None => 1024,
+    };
 
     match bytes_exist {
         Some(bytes_as_string) => {
             // This should be able to handle an unreasonable number of bytes.
-            let num_bytes = bytes_as_string.trim().parse::<BigInt>().expect("Failed to understand supplied value of bytes to write");
+            let num_bytes = bytes_as_string.trim().parse::<BigInt>().expect("Failed to understand supplied value of bytes");
             let max_loop = BigInt::from_usize(usize::MAX).unwrap();
-            let num_iter = &num_bytes / (&max_loop * BigInt::from_usize(mem::size_of::<usize>()).unwrap());
-            let remaining_bytes = &num_bytes % (&max_loop * BigInt::from_usize(mem::size_of::<usize>()).unwrap());
-            let num_iter_remaining = &remaining_bytes / BigInt::from_usize(mem::size_of::<usize>()).unwrap();
-            let remainder = &remaining_bytes % BigInt::from_usize(mem::size_of::<usize>()).unwrap();
+            let num_iter = &num_bytes / (&max_loop * BigInt::from_usize(buffer_size).unwrap());
+            let remaining_bytes = &num_bytes % (&max_loop * BigInt::from_usize(buffer_size).unwrap());
+            let num_iter_remaining = &remaining_bytes / BigInt::from_usize(buffer_size).unwrap();
+            let remainder = &remaining_bytes % BigInt::from_usize(buffer_size).unwrap();
             for _ in 0..(num_iter.to_usize().unwrap()) {
                 for _ in 0..(max_loop.to_usize().unwrap()) {
-                    rng_container.output_bytes().expect("Failed to write to stdout");
+                    RNG.with(|rng| {
+                        output_bytes(rng, buffer_size).expect("Failed to write to stdout");
+                    });
                 }
             }
             for _ in 0..(num_iter_remaining.to_usize().unwrap()) {
-                rng_container.output_bytes().expect("Failed to write to stdout");
+                RNG.with(|rng| {
+                    output_bytes(rng, buffer_size).expect("Failed to write to stdout");
+                });
             }
-            rng_container.output_less_than_usize_bytes(remainder.to_usize().unwrap()).expect("Failed to write to stdout");
+            RNG.with(|rng| {
+                output_bytes(rng, remainder.to_usize().unwrap()).expect("Failed to write to stdout");
+            });
         },
         None => {
             loop {
-                match rng_container.output_bytes() {
+                match RNG.with(|rng| output_bytes(rng, buffer_size)) {
                     Ok(_) => (),
                     Err(_) => break,
                 }
             }
-        }
+        },
     }
 }
